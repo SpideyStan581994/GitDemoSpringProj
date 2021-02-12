fea2c410ccb8add9aac9a68a1b729b29672a26b9
SpideyStan581994 <steven.tan@ulaps.dev>
2021-02-12 17:37:19 +0800

    Added  name parameter to display custom text
    adding @RequestParam on the method sayHello and bind it on the String variable on the method.

--- src/main/java/com/ulap/demo/controller/HelloWorldController.java
+++ src/main/java/com/ulap/demo/controller/HelloWorldController.java
@@ -2,15 +2,16 @@ package com.ulap.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HelloWorldController {
 
-    @GetMapping
-    public String sayHello(){
-        return "Hello World.";
+    @GetMapping("/greeting")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "hello") String name){
+        return "Hello " + name;
     }
     
 }
---
+++ src/main/java/com/ulap/demo/controller/PersonModel.java
@@ -0,0 +1,51 @@
+package com.ulap.demo.controller;
+
+public class PersonModel {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public PersonModel() {
+    }
+
+    public PersonModel(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", age='" + getAge() + "'" +
+            "}";
+    }
+    
+}
